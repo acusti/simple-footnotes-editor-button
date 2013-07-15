3ace193dc3e7acb081dbad705562b203d2fe1a52
Andrew Patton <andrew@acusti.ca>
2013-07-15 13:42:51 -0400

    Plugin comment header formatting change

--- simple-footnotes-editor-button.php
+++ simple-footnotes-editor-button.php
@@ -1,15 +1,16 @@
 <?php
 /*
- * Plugin Name: Simple Footnotes Editor Button
- * Plugin URI: http://wordpress.org/extend/plugins/simple-footnotes-editor-button/
- * Plugin Description: Adds a button to the TinyMCE editor toolbar in the second row that makes it easy for users to add footnotes with the proper syntax.
- * Version: 0.21
- * Author: Andrew Patton
- * Author URI: http://www.purecobalt.com/
- * License: CC0
- * Text Domain: simple-footnotes-editor-button
- * Domain Path: languages
- */
+Plugin Name: Simple Footnotes Editor Button
+Plugin URI: http://wordpress.org/extend/plugins/simple-footnotes-editor-button/
+Plugin Description: Adds a button to the TinyMCE editor toolbar in the second row that makes it easy for users to add footnotes with the proper syntax.
+Version: 0.21
+Author: Andrew Patton
+Author URI: http://www.purecobalt.com/
+License: CC0
+Text Domain: simple-footnotes-editor-button
+Domain Path: languages/
+*/
+
 class simple_footnotes_editor_button {
 
 	/**
